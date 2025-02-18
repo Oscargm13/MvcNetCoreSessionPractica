9e3d4a220721a0386daefeacbdde3c14c7386a1b
Oscargm13 <oskitargm5@gmail.com>
2025-02-18 10:02:39 +0100

    Add project files.

---
+++ MvcNetCoreSessionPractica/Controllers/EjemploSessionController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCoreSessionPractica.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SessionSimple(string accion)
+        {
+            if(accion != null)
+            {
+                if(accion.ToLower() == "almacenar")
+                {
+                    HttpContext.Session.SetString("nombre", "Programeitor");
+                    HttpContext.Session.SetString("hora", DateTime.Now.ToLongTimeString());
+                    ViewData["MENSAJE"] = "Datos almacenados session";
+                }else if (accion.ToLower() == "mostrar")
+                {
+                    ViewData["USUARIO"] = HttpContext.Session.GetString("nombre");
+                    ViewData["HORA"] = HttpContext.Session.GetString("hora");
+                }
+            }
+            return View();
+        }
+    }
+}
